bd4e45eb3e765146c98ece31cbc093575e64f72b
uu-space <131173052+uu-space@users.noreply.github.com>
2024-01-31 08:05:05 +0800

    update: app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import Koa from 'koa';
 import Router from '@koa/router';
 import bodyParser from 'koa-bodyparser';
 
+const port = 7890;
 const app = new Koa();
 const router = new Router();
 
@@ -17,6 +18,6 @@ router.get('/', async (ctx) => {
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(7860, () => {
+app.listen(port, () => {
   console.log('Server is running on http://localhost:3000');
 });
